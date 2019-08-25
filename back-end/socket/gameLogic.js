df4b2ab6923fa152ce474e69a5236590a20c1848
Nghi Pham <ptnghi@apcs.vn>
2019-08-25 10:12:22 +0700

    minor fix in disconnect logic

--- back-end/socket/gameLogic.js
+++ back-end/socket/gameLogic.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var roomList = [];
 var roomListInfo = {};
 const userService = require('../service/UserService');
@@ -16,9 +15,9 @@ const gameLogic = function(io){
     let roomId = "";
 
     const leaveRoom = (socket) => {
+      if  (roomId){
         socket.leave(roomId);
         let userList = roomList[roomId].userList;
-        delete roomList[roomId];
         io.in('global').emit('room-close', {
             id: roomId
         });
@@ -29,6 +28,8 @@ const gameLogic = function(io){
         let index = userList.indexOf(socket.id);
         userList.slice(index,1);
         let leftUserid = userList[0];
+        let oldName = roomListInfo[roomId].name;
+        delete roomList[roomId];
         let newRoom ={
             userList: userList,
             currTurn: 0,
@@ -45,7 +46,7 @@ const gameLogic = function(io){
                     username: params.username,
                     rank: params.rank
                 },
-                name: data.roomName,
+                name: oldName,
                 createdAt: date,
                 status: 0
             };
@@ -55,7 +56,9 @@ const gameLogic = function(io){
         };
         userService.getUser(leftUserid, callback);
 
-        roomId=""; 
+        roomId="";
+      }
+
     };
 
     io.use(function(socket, next){
@@ -67,18 +70,18 @@ const gameLogic = function(io){
           });
         } else {
             next(new Error('Authentication error'));
-        }    
+        }
     })
     .on('connection', (socket) => {
         console.log('New user connected');
         socket.join('global');
         console.log(socket.decoded);
-        
+
         setTimeout(()=>{
             console.log(roomListInfo);
             socket.emit('room-list', roomListInfo);
         },500);
-        
+
         socket.on('create-room', (data) => {
             socket.join(socket.id);
             roomId = socket.id;
@@ -131,12 +134,12 @@ const gameLogic = function(io){
                     id: roomId
                 });
             };
-            
+
             userService.getUser(socket.id, callback);
-            
-            
-        }); 
-    
+
+
+        });
+
         socket.on('game-ack', () => {
             roomList[roomId].start_ack++;
             console.log(roomList[roomId].start_ack);
@@ -172,7 +175,7 @@ const gameLogic = function(io){
                 socket.to('global').emit('room-full', {
                     id: roomId
                 });
-                msgBody.gameEnd = 0;   
+                msgBody.gameEnd = 0;
             }
 
             socket.to(roomId).emit('turn', msgBody);
@@ -191,218 +194,16 @@ const gameLogic = function(io){
         socket.on('chat', (data) => {
             socket.to(roomId).emit('chat', data.msg);
         });
-        
+
         socket.on('leave-room', () => {
              leaveRoom(socket);
         });
 
         socket.on(('disconnect'), (reason) => {
-            // leaveRoom(socket);
-            console.log("Disconnenct because " + reason); 
+            leaveRoom(socket);
+            console.log("Disconnenct because " + reason);
         });
     });
 
 };
-=======
-var roomList = [];
-var roomListInfo = [];
-const userService = require('../service/UserService');
-const secret="pGctNMl4LL4bEQSwCdIzdg";
-const jwt = require('jsonwebtoken');
-
-const getCurrentDate = () => {
-    let today = new Date();
-    let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-    let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    return date+' '+time;
-};
-
-const gameLogic = function(io){
-    let roomId = "";
-
-    const leaveRoom = (socket) => {
-        socket.leave(roomId);
-        let userList = roomList[roomId].userList;
-        delete roomList[roomId];
-        io.in('global').emit('room-close', {
-            id: roomId
-        });
-        if (roomList[roomId].status === 2){
-            let winner = (userList.indexOf(socket.id) + 1) % 2;
-            userService.updateRank(userList[winner], socket.id);
-        }
-        let index = userList.indexOf(socket.id);
-        userList.slice(index,1);
-        let leftUserid = userList[0];
-        let newRoom ={
-            userList: userList,
-            currTurn: 0,
-            status : 0,
-            start_ack: 0
-        };
-        roomList[leftUserid] = newRoom;
-
-        let date = getCurrentDate();
-        let callback = (params) => {
-            let newRoom = {
-                id: leftUserid,
-                creator: {
-                    username: params.username,
-                    rank: params.rank
-                },
-                name: data.roomName,
-                createdAt: date,
-                status: 0
-            };
-            roomListInfo[socket.id] = newRoom
-            socket.to('global').emit('new-room', newRoom);
-            socket.to(roomId).emit('other-disconnect',newRoom);
-        };
-        userService.getUser(leftUserid, callback);
-
-        roomId=""; 
-    };
-
-    io.use(function(socket, next){
-        if (socket.handshake.query && socket.handshake.query.token){
-          jwt.verify(socket.handshake.query.token, secret, function(err, decoded) {
-            if(err) return next(new Error('Authentication error'));
-            socket.id = decoded.id;
-            next();
-          });
-        } else {
-            next(new Error('Authentication error'));
-        }    
-    })
-    .on('connection', (socket) => {
-        console.log('New user connected');
-        socket.join('global');
-        console.log(socket.decoded);
-
-        socket.emit('room-list', roomListInfo);
-        socket.on('create-room', (data) => {
-            socket.join(socket.id);
-            roomId = socket.id;
-            let newRoom ={
-                userList: [socket.id],
-                currTurn: 0,
-                status : 0,
-                start_ack: 0
-            };
-            roomList[socket.id] = newRoom;
-            socket.emit("room-created", {
-                id: socket.id
-            });
-            console.log(roomList);
-            let date = getCurrentDate();
-            let callback = (params) => {
-                let newRoom = {
-                    id: socket.id,
-                    creator: {
-                        username: params.username,
-                        rank: params.rank
-                    },
-                    name: data.roomName,
-                    createdAt: date,
-                    status: 0
-                };
-                roomListInfo[socket.id] = newRoom
-                socket.to('global').emit('new-room', newRoom);
-            };
-            userService.getUser(socket.id, callback);
-
-        });
-        socket.on('join', (data) => {
-            roomId = data.roomId;
-            socket.join(roomId);
-
-            console.log(`${socket.id} has joined room ${data.roomId}`);
-
-            roomList[roomId].userList.push(socket.id);
-            roomList[roomId].status = 1;
-
-            console.log(roomList[roomId].userList);
-
-            let callback = (joinedUser)=>{
-                io.in(roomId).emit('start-game', {joining: {
-                    username: joinedUser.username,
-                    rank: joinedUser.rank
-                }});
-                socket.to('global').emit('room-full', {
-                    id: roomId
-                });
-            };
-            
-            userService.getUser(socket.id, callback);
-            
-            
-        }); 
-    
-        socket.on('game-ack', () => {
-            roomList[roomId].start_ack++;
-            console.log(roomList[roomId].start_ack);
-            if (roomList[roomId].start_ack === 2){
-                roomList[roomId].status = 2;
-                roomList[roomId].currTurn = Math.floor((Math.random() * 2));
-                let currUser = roomList[roomId].userList[roomList[roomId].currTurn % 2];
-                io.in(roomId).emit('start-playing', {'message': 'PLAY DI'});
-                setTimeout(() => {
-                    io.in(roomId).emit('turn', {user: currUser, currTurn: roomList[roomId].currTurn, firstTurn: 1});
-                    socket.to('global').emit('room-start-playing', {
-                        id: roomId
-                    });
-                    roomList[roomId].currTurn++;
-                    console.log('Start at: ' + currUser);
-                }, 2000);
-            }
-        });
-
-        socket.on('play', (data) => {
-            let currUser = roomList[roomId].userList[roomList[roomId].currTurn % 2];
-            let msgBody = {
-                user: currUser,
-                currTurn: roomList[roomId].currTurn,
-                updatedBoard: data.updatedBoard
-            };
-
-            if (data.gameEnd === 1){
-                roomList[roomId].start_ack = 0;
-                msgBody.gameEnd = 1;
-                let winner = roomList[roomId].userList[(roomList[roomId].currTurn + 1) % 2];
-                userService.updateRank(winner,currUser);
-                socket.to('global').emit('room-full', {
-                    id: roomId
-                });
-                msgBody.gameEnd = 0;   
-            }
-
-            socket.to(roomId).emit('turn', msgBody);
-            roomList[roomId].currTurn++;
-        });
-
-        socket.on('replay', (data) => {
-            if (roomList[roomId].start_ack === 1){
-                roomList[roomId].start_ack = 0;
-                io.in(roomId).emit('start-game', {});
-            } else {
-                roomList[roomId].start_ack = 1;
-            }
-        });
-
-        socket.on('chat', (data) => {
-            socket.to(roomId).emit('chat', data.msg);
-        });
-        
-        socket.on('leave-room', () => {
-             leaveRoom(socket);
-        });
-
-        socket.on(('disconnect'), (reason) => {
-            // leaveRoom(socket);
-            console.log("Disconnenct because " + reason); 
-        });
-    });
-
-};
->>>>>>> a1f665a011c956deed05aa8873d00f56ed4b22ea
 module.exports = gameLogic;
